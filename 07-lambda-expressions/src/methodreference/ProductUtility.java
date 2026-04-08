fd691d0a6f60f499c42a7aa8fa77c0fa5e59d40e
Augusto <augusto.t.ibarrola@gmail.com>
2026-04-08 17:04:56 -0400

    07-lambda-expressions

--- 07-lambda-expressions/src/methodreference/ProductUtility.java
+++ 07-lambda-expressions/src/methodreference/ProductUtility.java
@@ -8,7 +8,8 @@ public class ProductUtility {
 	public static void main(String[] args) {
 		
 		List<String> productList = Arrays.asList("Frock", "Watch", "Shirt", "Violin");
-		productList.sort(String :: compareTo);
+		productList.sort(String::compareTo);
+		
 		for(String productName : productList) {
 			System.out.println(productName);
 		}
