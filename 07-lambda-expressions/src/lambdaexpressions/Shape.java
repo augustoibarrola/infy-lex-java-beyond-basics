e36e026b7b721b78e13e85ac1465f8a0a9aa2168
Augusto <augusto.t.ibarrola@gmail.com>
2026-04-08 14:48:32 -0400

    07-lambda-expressions

---
+++ 07-lambda-expressions/src/lambdaexpressions/Shape.java
@@ -0,0 +1,22 @@
+package lambdaexpressions;
+
+@FunctionalInterface
+public interface Shape {
+
+	public void draw();
+
+//	public void area();
+
+	public default Double area(Integer radius) {
+		return radius * 3.14;
+	}
+
+	public default void fill(String color) {
+		System.out.println("Inside default method fill()");
+	}
+
+	public static void clean() {
+		System.out.println("Inside static method clean()");
+	}
+
+}
---
+++ 07-lambda-expressions/src/lambdaexpressions/ShapeClient.java
@@ -0,0 +1,9 @@
+package lambdaexpressions;
+
+public class ShapeClient {
+
+	public static void main(String[] args) {
+		Shape.clean();
+	}
+	
+}
