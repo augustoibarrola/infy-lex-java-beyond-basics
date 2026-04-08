980894c350fdf96f0535024a16de875ad11c009a
Augusto <augusto.t.ibarrola@gmail.com>
2026-04-08 17:03:57 -0400

    07-lambda-expressions

--- 07-lambda-expressions/src/lambdaexpressions/ProductUtility.java
+++ 07-lambda-expressions/src/lambdaexpressions/ProductUtility.java
@@ -8,7 +8,11 @@ public class ProductUtility {
 	public static void main(String[] args) {
 		
 		List<String> productList = Arrays.asList("Frock", "Watch", "Shirt", "Violin");
-		productList.sort((String p1, String p2) -> p1.compareTo(p2));
+//		productList.sort((String p1, String p2) -> p1.compareTo(p2));
+//		productList.sort((p1, p2) -> p1.compareTo(p2));
+		productList.sort((p1, p2) -> {
+			return p1.compareTo(p2);
+		});
 		
 		for(String productName : productList){
 			System.out.println(productName);
---
+++ 07-lambda-expressions/src/methodreference/ProductUtility.java
@@ -0,0 +1,18 @@
+package methodreference;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ProductUtility {
+
+	public static void main(String[] args) {
+		
+		List<String> productList = Arrays.asList("Frock", "Watch", "Shirt", "Violin");
+		productList.sort(String :: compareTo);
+		for(String productName : productList) {
+			System.out.println(productName);
+		}
+
+	}
+
+}
